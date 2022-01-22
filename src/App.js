856a0bb7f56a820db5b72677d8d24e184193cedf
sbot19 <92232719+sbot19@users.noreply.github.com>
2022-01-21 22:45:01 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="dictionary-title">Dictionary</h1>>
+    </div>
+  );
+}
+
+export default App;
