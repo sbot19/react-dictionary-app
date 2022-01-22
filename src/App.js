5b1068a86cb2971ee42773681b006f3018ff535b
sbot19 <92232719+sbot19@users.noreply.github.com>
2022-01-21 22:47:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="dictionary-title">Dictionary</h1>>
+      <h1 className="dictionary-title">Dictionary</h1>
     </div>
   );
 }
